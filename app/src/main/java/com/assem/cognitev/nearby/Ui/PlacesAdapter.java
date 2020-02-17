c7cbced5fda1cc3ed2fadbd49ec6e8aa8b877ec4
Mohamed Assem <mo7mad.assim@gmail.com>
2020-02-17 12:07:21 +0200

    Implement PlacesAdapter

--- app/src/main/java/com/assem/cognitev/nearby/Ui/PlacesAdapter.java
+++ app/src/main/java/com/assem/cognitev/nearby/Ui/PlacesAdapter.java
@@ -1,17 +1,57 @@
 package com.assem.cognitev.nearby.Ui;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.assem.cognitev.nearby.Models.PlaceModel;
+import com.assem.cognitev.nearby.R;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class PlacesAdapter {
+public class PlacesAdapter extends RecyclerView.Adapter<PlacesAdapter.PlaceHolder> {
 
+    private Context context;
+    private ArrayList<PlaceModel> placeModelArrayList;
 
-    class PlaceHolder extends RecyclerView.ViewHolder{
+    public PlacesAdapter(Context context, ArrayList<PlaceModel> placeModelArrayList) {
+        this.context = context;
+        this.placeModelArrayList = placeModelArrayList;
+    }
 
+    @NonNull
+    @Override
+    public PlaceHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View item = LayoutInflater.from(context).inflate(R.layout.item_place, parent, false);
+        return new PlaceHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlaceHolder holder, int position) {
+        final PlaceModel placeModel = placeModelArrayList.get(position);
+        holder.placeTitle.setText(placeModel.getTitle());
+        holder.placeAddress.setText(placeModel.getAddress());
+    }
+
+    @Override
+    public int getItemCount() {
+        return placeModelArrayList.size();
+    }
+
+    class PlaceHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.item_place_title)
+        TextView placeTitle;
+        @BindView(R.id.item_place_address)
+        TextView placeAddress;
 
         public PlaceHolder(@NonNull View itemView) {
             super(itemView);

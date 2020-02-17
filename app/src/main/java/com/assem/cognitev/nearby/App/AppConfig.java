2e142f9209157f5a718aa8bb005c933f7cdb58b2
Mohamed Assem <mo7mad.assim@gmail.com>
2020-02-18 01:13:18 +0200

    Update AppConfig.java

--- app/src/main/java/com/assem/cognitev/nearby/App/AppConfig.java
+++ app/src/main/java/com/assem/cognitev/nearby/App/AppConfig.java
@@ -4,7 +4,7 @@ public class AppConfig {
 
     // END_POINTS
     /*
-https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=QI3JN03II2AEFXVUZWUWTDLUWBTRSHXJLPJDPWZ0V0QY2DA5&client_secret=JI23SHUX4JYSPJNLKDMYTZFEVSLBTX2KKVYVSTJKFBFIQKGZhttps://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=QI3JN03II2AEFXVUZWUWTDLUWBTRSHXJLPJDPWZ0V0QY2DA5&client_secret=JI23SHUX4JYSPJNLKDMYTZFEVSLBTX2KKVYVSTJKFBFIQKGZ&v=20200215&radius=500     */
+https://api.foursquare.com/v2/venues/search?ll=29.978391, س30.954928&client_id=QI3JN03II2AEFXVUZWUWTDLUWBTRSHXJLPJDPWZ0V0QY2DA5&client_secret=JI23SHUX4JYSPJNLKDMYTZFEVSLBTX2KKVYVSTJKFBFIQKGZhttps://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=QI3JN03II2AEFXVUZWUWTDLUWBTRSHXJLPJDPWZ0V0QY2DA5&client_secret=JI23SHUX4JYSPJNLKDMYTZFEVSLBTX2KKVYVSTJKFBFIQKGZ&v=20200215&radius=500     */
 
     public static final String BASE_URL = "https://api.foursquare.com/v2/venues/";
     public static final String LL = "ll";

1052afaa112f18b72cc4a18fc2081977730ebc91
Mohamed Assem <mo7mad.assim@gmail.com>
2020-02-17 12:20:44 +0200

    Update res

--- app/src/main/java/com/assem/cognitev/nearby/Ui/MainActivity.java
+++ app/src/main/java/com/assem/cognitev/nearby/Ui/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        init();
     }
 
     private void init() {

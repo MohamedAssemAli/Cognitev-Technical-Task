783e132a9afe7a66fcf30dd3ccd48368a4398b86
Mohamed Assem <mo7mad.assim@gmail.com>
2020-02-18 20:54:35 +0200

    Update AppConfig.java

--- app/src/main/java/com/assem/cognitev/nearby/App/AppConfig.java
+++ app/src/main/java/com/assem/cognitev/nearby/App/AppConfig.java
@@ -4,9 +4,16 @@ public class AppConfig {
 
     // END_POINTS
     /*
-https://api.foursquare.com/v2/venues/search?ll=29.978391, س30.954928&client_id=QI3JN03II2AEFXVUZWUWTDLUWBTRSHXJLPJDPWZ0V0QY2DA5&client_secret=JI23SHUX4JYSPJNLKDMYTZFEVSLBTX2KKVYVSTJKFBFIQKGZhttps://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=QI3JN03II2AEFXVUZWUWTDLUWBTRSHXJLPJDPWZ0V0QY2DA5&client_secret=JI23SHUX4JYSPJNLKDMYTZFEVSLBTX2KKVYVSTJKFBFIQKGZ&v=20200215&radius=500     */
+https://api.foursquare.com/v2/venues/search?ll=29.978391, 30.954928&client_id=QI3JN03II2AEFXVUZWUWTDLUWBTRSHXJLPJDPWZ0V0QY2DA5&client_secret=JI23SHUX4JYSPJNLKDMYTZFEVSLBTX2KKVYVSTJKFBFIQKGZhttps://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=QI3JN03II2AEFXVUZWUWTDLUWBTRSHXJLPJDPWZ0V0QY2DA5&client_secret=JI23SHUX4JYSPJNLKDMYTZFEVSLBTX2KKVYVSTJKFBFIQKGZ&v=20200215&radius=500
 
-    public static final String BASE_URL = "https://api.foursquare.com/v2/venues/";
+     https://api.foursquare.com/v2/venues/explore?ll=29.978391,30.954928
+     &client_id=QI3JN03II2AEFXVUZWUWTDLUWBTRSHXJLPJDPWZ0V0QY2DA5
+     &client_secret=JI23SHUX4JYSPJNLKDMYTZFEVSLBTX2KKVYVSTJKFBFIQKGZ
+     &v=20200215
+     &radius=500
+     */
+
+    public static final String BASE_URL = "https://api.foursquare.com/v2/";
     public static final String LL = "ll";
     public static final String CLIENT_ID = "client_id";
     public static final String ID = "QI3JN03II2AEFXVUZWUWTDLUWBTRSHXJLPJDPWZ0V0QY2DA5";
@@ -17,3 +24,14 @@ https://api.foursquare.com/v2/venues/search?ll=29.978391, س30.954928&client_id=
 
 
 }
+
+
+/*
+    @GET("venues/explore")
+    public Call<Explore> getPlaces
+            (@Query(AppConfig.CLIENT_ID) String clientId,
+             @Query(AppConfig.CLIENT_SECRET) String clientSecret,
+             @Query(AppConfig.LL) String lat,
+             @Query(AppConfig.RADIUS) String radius,
+             @Query(AppConfig.V) String date);
+ */
